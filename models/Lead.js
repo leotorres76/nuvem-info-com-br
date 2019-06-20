afb15efdf05e5560055e06343de7cbbbc1da3c2c
Leo Torres <lemarto@gmail.com>
2019-06-20 17:29:45 -0300

    inserção do campo tipo no csv e firebase

--- models/Lead.js
+++ models/Lead.js
@@ -1,19 +1,21 @@
 const firebase = require('../firebase');
 const stringify = require ('csv-stringify');
 
-const create = ({ email, name, ipAddress, date }) => {
+//essa função grava os campos no firebase
+const create = ({ email, name, ipAddress, tipo, date }) => {
     const leads = firebase.database().ref('leads');
-    const lead = leads.push({ email, name, ipAddress, date });
+    const lead = leads.push({ email, name, ipAddress, tipo, date });
     return lead;
 };
 
+//cria arquivo csv com os dados do firebase
 const csv = (cb) => {
     const leads = firebase.database().ref('leads'); 
-    const data = [['id', 'email', 'name', 'ip', 'data_hora']];
+    const data = [['id', 'email', 'name', 'ip', 'tipo', 'data_hora']];
     leads.on('value', (snapshot) => {
         snapshot.forEach((lead) => {
             const { email, name, ipAddress, date } = lead.val();
-            data.push([lead.key, email, name, ipAddress, date]);
+            data.push([lead.key, email, name, ipAddress, tipo, date]);
         });
         stringify(data, (err, output) => {
             cb(output);
