af09d5ecbf35dafd592bf4e9818c69b9dea18ca1
Leo Torres <lemarto@gmail.com>
2019-06-20 15:22:10 -0300

    atualização com inclusão getIp

--- index.js
+++ index.js
@@ -5,43 +5,6 @@ const ip = require('ip');
 
 const app = express ();
 
-function getIp(callback)
-{
-    function response(s)
-    {
-        callback(window.userip);
-
-        s.onload = s.onerror = null;
-        document.body.removeChild(s);
-    }
-
-    function trigger()
-    {
-        window.userip = false;
-
-        var s = document.createElement("script");
-        s.async = true;
-        s.onload = function() {
-            response(s);
-        };
-        s.onerror = function() {
-            response(s);
-        };
-
-        s.src = "https://l2.io/ip.js?var=userip";
-        document.body.appendChild(s);
-    }
-
-    if (/^(interactive|complete)$/i.test(document.readyState)) {
-        trigger();
-    } else {
-        document.addEventListener('DOMContentLoaded', trigger);
-    }
-}
-
-getIp(function (ip) {
-    return ip;
-});
 
 app.use(express.static('assets'));
 
@@ -79,7 +42,7 @@ app.get('/obrigado', (req, res) => {
 app.post('/leads', (req, res) => {
     const name = req.body.lead.name; //pega o campo name do form e leva pro Lead.js
     const email = req.body.lead.email; //pega o campo email do form e leva pro Lead.js
-    const ipAddress = getIp(ip);
+    const ipAddress = req.body.lead.myIp; //pega o ip pela função getIp do index.html
 	const date = new Date(Date.now()).toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' }); // pega a data e leva pro Lead.js
 	const lead = Lead.create({ name, email, ipAddress, date }); //cria no firebase com a função Lead
 	res.sendFile(__dirname + '/public/obrigado.html'); //aqui devolve algo depois de gravar no firebase pode ser a pagina de obrigado.html ou msg na mesma pagina
